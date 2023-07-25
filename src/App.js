15ae31a60da2048f4c585ca336061faf42a61e0e
andy olsen <leak187@gmail.com>
2023-07-25 13:08:00 -0700

    init commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import UserPedalInputs from './components/UserPedalInputs';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Guitar Steero</h1>
+      </header>
+      <section>
+        <UserPedalInputs />
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/UserPedalInputs/index.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+import { VictoryChart, VictoryLine, VictoryTheme } from 'victory';
+
+const UserPedalInputs = () => {
+  const [ currentController, setCurrentController ] = useState(null)
+  const [ throttle, setThrottle ] = useState(0)
+  const [ brake, setBrake ] = useState(0)
+  const [ steering, setSteering ] = useState(0)
+  const [ data, setData ] = useState({ throttle: [], brake: [] })
+
+  const scaleInput = (value) => {
+    return ((value * -1) + 1) * 50;  // scales and shifts value from range [-1, 1] to [0, 100]
+  }
+
+  const checkForControllers = () => {
+    let gamepads = navigator.getGamepads();
+    for(let i = 0; i < gamepads.length; i++) {
+      let gamepad = gamepads[i];
+      if(gamepad) {
+        setCurrentController(gamepad.id)
+        setThrottle(scaleInput(gamepad.axes[2]));
+        setBrake(scaleInput(gamepad.axes[5]));
+        setSteering(scaleInput(gamepad.axes[0]));
+        setData(oldData => {
+          const throttleData = [...oldData.throttle, { x: Date.now(), y: scaleInput(gamepad.axes[2]) }];
+          const brakeData = [...oldData.brake, { x: Date.now(), y: scaleInput(gamepad.axes[5]) }];
+          return { throttle: throttleData.slice(-50), brake: brakeData.slice(-50) }; // Keeps the last 50 data points
+        });
+      }
+    }
+  }
+
+  const logController = () => {
+    let gamepads = navigator.getGamepads();
+    for(let i = 0; i < gamepads.length; i++) {
+      let gamepad = gamepads[i];
+      if(gamepad) {
+        console.log(gamepad)
+      }
+    }
+  }
+
+  // Set up an interval to continuously check for controller updates
+  useEffect(() => {
+    const intervalId = setInterval(checkForControllers, 100); // Polling every 1000ms
+    return () => clearInterval(intervalId); // Clear the interval when the component unmounts
+  }, []);
+
+  return (
+    <div>
+      <button onClick={checkForControllers}>refresh controllers</button>
+      <button onClick={logController}>log controller</button>
+      <div>Your Inputs ({currentController}):</div>
+      <div>brake: {brake.toFixed(2)}%</div>
+      <div>throttle: {throttle.toFixed(2)}%</div>
+      <div>steering: {steering.toFixed(2)}%</div>
+      <VictoryChart theme={VictoryTheme.material} scale={{ x: "time" }}>
+        <VictoryLine data={data.throttle} style={{ data: { stroke: "red" }}}/>
+        <VictoryLine data={data.brake} style={{ data: { stroke: "blue" }}}/>
+      </VictoryChart>
+    </div>
+  )
+}
+
+export default UserPedalInputs
